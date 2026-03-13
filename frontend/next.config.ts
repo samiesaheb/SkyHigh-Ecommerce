8f333ac640027d58954a61e470526ade73277d79
Samie Saheb <shs.priv@gmail.com>
2026-03-13 11:37:21 +0700

    Fixed problem with images not showing

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -70,6 +70,7 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512],
     minimumCacheTTL: 31536000, // 1 year cache
+    dangerouslyAllowLocalIP: process.env.NODE_ENV === 'development',
     dangerouslyAllowSVG: false,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
